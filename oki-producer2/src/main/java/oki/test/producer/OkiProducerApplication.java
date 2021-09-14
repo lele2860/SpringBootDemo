7ec4c63d335771169445e8c09b920c67d47dc656
Oki <570548315@qq.com>
2021-09-14 21:30:10 +0800

    服务消费者

---
+++ oki-producer2/src/main/java/oki/test/producer/OkiProducerApplication.java
@@ -0,0 +1,15 @@
+package oki.test.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class OkiProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OkiProducerApplication.class, args);
+    }
+
+}
---
+++ oki-producer2/src/test/java/oki/test/producer/OkiProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package oki.test.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkiProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
