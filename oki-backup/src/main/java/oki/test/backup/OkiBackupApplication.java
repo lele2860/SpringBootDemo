1d5a5212d3377a0cb7dfa6d982ba55ae159a6391
Oki <570548315@qq.com>
2021-09-14 17:23:10 +0800

    consul配置

--- oki-backup/src/main/java/oki/test/backup/OkiBackupApplication.java
+++ oki-backup/src/main/java/oki/test/backup/OkiBackupApplication.java
@@ -2,7 +2,9 @@ package oki.test.backup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OkiBackupApplication {
 
